3cc6ddf35ba5ad28ea6738da25988ba74a2576c5
Aum Trivedi <trivediaum0013@gmail.com>
2018-08-24 13:47:54 -0700

    first commit

---
+++ inc/theme-options.php
@@ -0,0 +1,161 @@
+	
+<?php
+/**
+ * Initialize the custom theme options.
+ */
+add_action( 'admin_init', 'custom_theme_options' );
+
+/**
+ * Build the custom settings & update OptionTree.
+ */
+function custom_theme_options() {
+  
+  /* OptionTree is not loaded yet */
+  if ( ! function_exists( 'ot_settings_id' ) )
+    return false;
+    
+  /**
+   * Get a copy of the saved settings array. 
+   */
+  $saved_settings = get_option( ot_settings_id(), array() );
+  
+  /**
+   * Custom settings array that will eventually be 
+   * passes to the OptionTree Settings API Class.
+   */
+  $custom_settings = array( 
+    'contextual_help' => array( 
+      'sidebar'       => ''
+    ),
+    'sections'        => array( 
+      array(
+        'id'          => 'general',
+        'title'       => 'General'
+      ),
+      array(
+        'id'          => 'logo',
+        'title'       => 'Logo'
+      )
+    ),
+    'settings'        => array( 
+      array(
+        'id'          => 'footer_text',
+        'label'       => 'Footer Text',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'text',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'facebook',
+        'label'       => 'Facebook',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'text',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'twitter',
+        'label'       => 'Twitter',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'text',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'linkedin',
+        'label'       => 'Linkedin',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'text',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'rss',
+        'label'       => 'RSS',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'text',
+        'section'     => 'general',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'mainlogo',
+        'label'       => 'Mainlogo:Size(275px X 85px)',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'logo',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      ),
+      array(
+        'id'          => 'footerogo',
+        'label'       => 'Footerlogo:Size(275px X 85px)',
+        'desc'        => '',
+        'std'         => '',
+        'type'        => 'upload',
+        'section'     => 'logo',
+        'rows'        => '',
+        'post_type'   => '',
+        'taxonomy'    => '',
+        'min_max_step'=> '',
+        'class'       => '',
+        'condition'   => '',
+        'operator'    => 'and'
+      )
+    )
+  );
+  
+  /* allow settings to be filtered before saving */
+  $custom_settings = apply_filters( ot_settings_id() . '_args', $custom_settings );
+  
+  /* settings are not the same update the DB */
+  if ( $saved_settings !== $custom_settings ) {
+    update_option( ot_settings_id(), $custom_settings ); 
+  }
+  
+  /* Lets OptionTree know the UI Builder is being overridden */
+  global $ot_has_custom_theme_options;
+  $ot_has_custom_theme_options = true;
+  
+}
